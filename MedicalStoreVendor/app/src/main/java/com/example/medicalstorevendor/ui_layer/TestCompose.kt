0baf1aceb1750e97626c57289c7cea25205c99c9
veenus nishad <veenusnishad@proton.me>
2024-10-06 00:09:31 +0530

    6th OCT

---
+++ MedicalStoreVendor/app/src/main/java/com/example/medicalstorevendor/ui_layer/TestCompose.kt
@@ -0,0 +1,15 @@
+package com.example.medicalstorevendor.ui_layer
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showSystemUi = true)
+@Composable
+fun TestCompose(modifier: Modifier = Modifier){
+    LazyColumn {
+      
+    }
+}
