99b0cf45bd820fad66ab89a159a2d9d8de25ad24
Veenus-Nishad <veenusnishad@proton.me>
2024-12-03 23:46:36 +0530

    I hate you am 03-12-2024

--- PracticeCompose/app/src/main/java/com/example/practicecompose/sideEffects/LaunchedEffectPractice.kt
+++ PracticeCompose/app/src/main/java/com/example/practicecompose/sideEffects/LaunchedEffectPractice.kt
@@ -1,11 +1,12 @@
 package com.example.practicecompose.sideEffects
 
-import android.annotation.SuppressLint
+import android.nfc.Tag
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,41 +18,63 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 
-@SuppressLint("UnrememberedMutableState")
+ enum class CounterState{
+        Started,inProgress,Finished
+}
+
 @Composable
 fun StartTimer() {
 
-    var isTimerComplete by remember { mutableStateOf(false) }
+    var counterState by remember { mutableIntStateOf(0) }
 
-    var countup by remember { mutableIntStateOf(0) }
-    var color = listOf(
+    var isCounterFinished by remember { mutableStateOf(false) }
+
+    var bgColor by remember { mutableStateOf(Color.White) }
+
+    var colors = listOf(
         Color.Blue,
-        Color.Red,
         Color.Gray,
+        Color.Red,
         Color.Green,
+        Color.Yellow,
+        Color.Magenta,
+        Color.Cyan,
         Color.Black,
+        Color.Blue,
+
     )
-    var lastIndex=color.lastIndex
-    var bgcolor by remember { mutableStateOf(Color.White)}
-    LaunchedEffect(true) {
-            color.forEach {
-                delay(1000)
-                bgcolor=it
-                Log.d("Tag",bgcolor.toString())
-                if (it==color[lastIndex]) {
-                    isTimerComplete = true
-                } else{countup++}
-            }
-          }
+
+    LaunchedEffect(Unit) { // unit for only one whole execution of forEach Loop agar counter State se karte toh 8 baar call hota Launched effect
+
+        colors.forEach { color->
+            delay(1000)
+            bgColor=color
+        }
+        counterState++
+        isCounterFinished=true
+
+        Log.d("TAG","LaunchedEffect Launched ")
+    }
+
+
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = bgcolor),
+        modifier=Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {if(isTimerComplete)Text("My countdown is over ")  else Text("${countup}")
+    ) {
+        if (isCounterFinished){
+        Text(
+            "Counter is Finished"
+
+        )}
+        else{
+            Text(
+            text =  counterState.toString() ,
+            modifier = Modifier.background(bgColor).size(60.dp))
+        }
     }
 }
 
